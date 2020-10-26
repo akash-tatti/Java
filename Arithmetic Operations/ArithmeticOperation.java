dd5c55bdba83d62501fc19fdda4cb9340bed09c1
Akash <akashtatti2904@gmail.com>
2020-10-26 22:10:16 +0530

    sample1

---
+++ Arithmetic Operations/ArithmeticOperation.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+public class ArithmeticOperation {
+
+   public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+      
+        System.out.println("Enter first number :");
+        int int1 = in.nextInt();
+        System.out.println("Enter second number :");
+        int int2 = in.nextInt();
+        
+
+        // Perform arithmetic operations.
+        int sum 		= int1 + int2;
+        int difference 	= int1 - int2;
+        int product 	= int1 * int2;
+        int quotient	= int1 / int2;
+        int modulo	    = int1 % int2;
+        
+
+        // Print result to console.
+        System.out.println("Sum : "         + sum);
+        System.out.println("Difference : "  + difference);
+        System.out.println("Product : "     + product);
+        System.out.println("Quotient : "    + quotient);
+        System.out.println("Modulo : "      + modulo);
+
+
+
+
+
+       
+
+
+        System.out.println("Enter first number :");
+        short short1 = in.nextShort();
+        System.out.println("Enter second number :");
+        short short2 = in.nextShort();
+        
+
+        // Perform arithmetic operations.
+        short sum 		= short1 + short2;
+        short difference 	= short1 - short2;
+        short product 	= short1 * short2;
+        short quotient	= short1 / short2;
+        short modulo	    = short1 % short2;
+        
+
+        // Print result to console.
+        System.out.println("Sum : "         + sum);
+        System.out.println("Difference : "  + difference);
+        System.out.println("Product : "     + product);
+        System.out.println("Quotient : "    + quotient);
+        System.out.println("Modulo : "      + modulo);
+
+
+
+
+
+
+
+        System.out.println("Enter first number :");
+        long long1 = in.nextLong();
+        System.out.println("Enter second number :");
+        long long2 = in.nextLong();
+        
+
+        // Perform arithmetic operations.
+        long sum 		= long1 + long2;
+        long difference 	= long1 - long2;
+        long product 	= long1 * long2;
+        long quotient	= long1 / long2;
+        long modulo	    = long1 % long2;
+        
+
+        // Print result to console.
+        System.out.println("Sum : "         + sum);
+        System.out.println("Difference : "  + difference);
+        System.out.println("Product : "     + product);
+        System.out.println("Quotient : "    + quotient);
+        System.out.println("Modulo : "      + modulo);
+
+
+
+        System.out.println("Enter first number :");
+        float float1 = in.nextFloat();
+        System.out.println("Enter second number :");
+        float float2 = in.nextFloat();
+        
+
+        // Perform arithmetic operations.
+        float sum 		= float1 + float2;
+        float difference 	= float1 - float2;
+        float product 	= float1 * float2;
+        float quotient	= float1 / float2;
+        float modulo	    = float1 % float2;
+        
+
+        // Print result to console.
+        System.out.println("Sum : "         + sum);
+        System.out.println("Difference : "  + difference);
+        System.out.println("Product : "     + product);
+        System.out.println("Quotient : "    + quotient);
+        System.out.println("Modulo : "      + modulo);
+
+
+
+
+
+
+        System.out.println("Enter first number :");
+        double double1 = in.nextDouble();
+        System.out.println("Enter second number :");
+        double double2 = in.nextDouble();
+        
+
+        // Perform arithmetic operations.
+        double sum 		= double1 + double2;
+        double difference 	= double1 - double2;
+        double product 	= double1 * double2;
+        double quotient	= double1 / double2;
+        double modulo	    = double1 % double2;
+        
+
+        // Print result to console.
+        System.out.println("Sum : "         + sum);
+        System.out.println("Difference : "  + difference);
+        System.out.println("Product : "     + product);
+        System.out.println("Quotient : "    + quotient);
+        System.out.println("Modulo : "      + modulo);
+
+
+
+
+
+
+    }
+}
